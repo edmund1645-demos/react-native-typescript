c2084f8d43ebf2308be9d01a505233d6ff82c364
Edmund Ekott <edmund.timfon@gmail.com>
2021-04-07 16:52:42 +0100

    initial commit

---
+++ App.tsx
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, View, FlatList} from 'react-native';
+
+import Header from './src/components/Header';
+import AddItem, {IItem} from './src/components/AddItem';
+import Item from './src/components/Item';
+const App = () => {
+  const [shoppingList, setShoppingList] = useState<IItem[]>([]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header title="Shopping List" />
+      <View style={styles.contentWrapper}>
+        <AddItem
+          setShoppingList={setShoppingList}
+          shoppingList={shoppingList}
+        />
+
+        <FlatList
+          data={shoppingList}
+          keyExtractor={(item, index) => `${item.item}-${index}`}
+          renderItem={({item}) => (
+            <Item item={item.item} quantity={item.quantity} />
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e8e7e3',
+  },
+  contentWrapper: {
+    padding: 20,
+  },
+});
+
+export default App;
---
+++ src/components/AddItem.tsx
@@ -0,0 +1,94 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+
+export interface IItem {
+  item: string;
+  quantity: string;
+}
+
+interface Props {
+  setShoppingList: React.Dispatch<React.SetStateAction<IItem[]>>;
+  shoppingList: IItem[];
+}
+
+const AddItem: React.FC<Props> = ({shoppingList, setShoppingList}) => {
+  const [item, setItem] = useState('');
+  const [quantity, setQuantity] = useState('');
+
+  const addItem = () => {
+    if (!item) {
+      Alert.alert('No Item!', 'You need to enter an item');
+    } else {
+      setShoppingList([
+        ...shoppingList,
+        {
+          item,
+          quantity: quantity || '1',
+        },
+      ]);
+
+      setItem('');
+      setQuantity('');
+    }
+  };
+
+  return (
+    <View>
+      <Text style={styles.heading}>Add Shopping Item</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter item"
+          value={item}
+          onChangeText={text => setItem(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter quantity"
+          keyboardType="numeric"
+          value={quantity}
+          onChangeText={q => {
+            setQuantity(q);
+          }}
+        />
+        <TouchableOpacity style={styles.addItemButton} onPress={addItem}>
+          <Text style={styles.buttonText}>Add Item</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 20,
+    fontWeight: '700',
+  },
+  form: {
+    marginTop: 30,
+  },
+  input: {
+    padding: 15,
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  addItemButton: {
+    backgroundColor: '#eb8634',
+    paddingVertical: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {color: '#fff', fontWeight: '500'},
+});
+
+export default AddItem;
